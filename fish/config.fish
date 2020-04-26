d42ae10498f8a2d8231e7c03056d2cf58f0d1dc0
Phillip Kemper <phillip-kemper@gmx.net>
2020-04-26 14:00:45 +0200

    fish config commit

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,30 @@
-/Users/phillip/.config/fish/config.fish
+echo Setting abbreviations... 
+echo Flutter Config
+export PATH="$PATH:/Users/phillip/development/flutter/bin"
+
+
+abbr g 'git'
+abbr ga 'git add'
+abbr gb 'git branch'
+abbr gbl 'git blame'
+abbr gc 'git commit -m'
+abbr gca 'git commit --amend -m'
+abbr gco 'git checkout'
+abbr gcp 'git cherry-pick'
+abbr gd 'git diff'
+abbr gf 'git fetch'
+abbr gl 'git log'
+abbr gm 'git merge'
+abbr gp 'git push'
+abbr gpf 'git push --force-with-lease'
+abbr gpl 'git pull'
+abbr gr 'git remote'
+abbr grb 'git rebase'
+abbr gs 'git status'
+abbr gst 'git stash'
+
+abbr k 'kubectl'
+abbr d 'docker'
+
+
+echo Done
